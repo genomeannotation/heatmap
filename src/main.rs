d487664fe9124e140f127024a320e07468c46e0d
Theodore DeRego <tderego94@gmail.com>
2015-09-04 14:45:51 -1000

    Fixed it

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,46 @@ use image::{
 };
 
 fn main() {
+    println!("Loading matrix...");
+
     let stdin = io::stdin();
     let mut matrix: Vec<Vec<f64>> = Vec::new();
-    let mut max_cell = 0.0;
     for line in stdin.lock().lines() {
         let mut row = Vec::new();
         let line = line.unwrap();
         for cell in line.trim_left().trim_right().split('\t') {
             let cell_val: f64 = cell.parse().unwrap();
             row.push(cell_val);
-            max_cell = f64::max(max_cell, cell_val);
         }
         matrix.push(row);
     }
 
-    let (width, height) = (matrix[0].len(), matrix.len());
+    // Build row and column sums
+    println!("Building sum vectors...");
+    let size = matrix.len();
+    let mut row_max = vec![0.0; size];
+    let mut col_max = vec![0.0; size];
 
-    //Construct a new ImageBuffer with the specified width and height.
-    let mut img = ImageBuffer::new(width as u32, height as u32);
+    for y in 0..size {
+        for x in 0..size {
+            row_max[y] = f64::max(row_max[y], matrix[y][x]);
+            col_max[x] = f64::max(col_max[x], matrix[y][x]);
+        }
+    }
 
-    for y in 0..height {
-        for x in 0..width {
+    // Build the image
+    println!("Building image...");
+
+    let mut img = ImageBuffer::new(size as u32, size as u32);
+
+    for y in 0..size {
+        for x in 0..size {
+            let max_cell =
+                if y > x {
+                    col_max[y]
+                } else {
+                    row_max[x]
+                };
             let cell = matrix[y][x]/max_cell * 255.0;
             img.put_pixel(x as u32, y as u32, Luma([cell as u8]));
         }
